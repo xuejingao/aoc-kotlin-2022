3eb9d8b563bde063afcec6112e71a4a09f380c2d
Xuejin Gao <xuejin.gao@aexp.com>
2022-12-02 15:18:39 -0500

    day 2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,9 +1,9 @@
 import Outcome.*
 
 private val selectedScoreMap = hashMapOf<String, Int>(
-        "X" to 1,
-        "Y" to 2,
-        "Z" to 3,
+        "X" to 1, // rock
+        "Y" to 2, // paper
+        "Z" to 3, // scissor
 )
 
 private enum class Outcome {
@@ -25,10 +25,10 @@ fun main() {
         else WIN
     }
 
-    fun calculateScore(opponentChoice: Char, yourChoice: Char): Int {
-        val result = determineResult(opponentChoice.toString(), yourChoice.toString())
+    fun calculateScore(opponentChoice: String, yourChoice: String): Int {
+        val result = determineResult(opponentChoice.toString(), yourChoice)
 
-        var yourScore = selectedScoreMap[yourChoice.toString()]?.plus(winScoreMap[result]!!)
+        var yourScore = selectedScoreMap[yourChoice]?.plus(winScoreMap[result]!!)
 
         return yourScore!!
     }
@@ -38,23 +38,71 @@ fun main() {
         var scoreFromFollowing = 0
 
         for( round in input ) {
-            val opponent = round[0]
-            val yours = round[2]
+            val opponent = round[0].toString()
+            val yours = round[2].toString()
 
             scoreFromFollowing += calculateScore(opponent, yours)
         }
         return scoreFromFollowing
     }
 
+    fun calculateYourSelection(opponentSelection: String, result: Outcome): String {
+        return if(result == DRAW) {
+            when(opponentSelection) {
+                "A" -> "X"
+                "B" -> "Y"
+                "C" -> "Z"
+                else -> throw error("invalid mapping")
+            }
+        } else if(result == WIN) {
+            when(opponentSelection) {
+                "A" -> "Y"
+                "B" -> "Z"
+                "C" -> "X"
+                else -> throw error("invalid mapping")
+            }
+        } else {
+            when(opponentSelection) {
+                "A" -> "Z"
+                "B" -> "X"
+                "C" -> "Y"
+                else -> throw error("invalid mapping")
+            }
+        }
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        // X = lose
+        // Y = Draw
+        // Z = WIN
+        // Calculate the points
+
+        var totalPoints = 0
+        for (round in input) {
+            val opponentsChoice = round[0].toString()
+            val endRoundResult = round[2].toString().let {
+                when(it) {
+                    "X" -> LOSE
+                    "Y" -> DRAW
+                    "Z" -> WIN
+                    else -> throw error("Not Valid Input")
+                }
+            }
+
+            val yourChoice = calculateYourSelection(opponentsChoice, endRoundResult)
+            totalPoints += calculateScore(opponentsChoice, yourChoice)
+
+        }
+
+        return totalPoints
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
